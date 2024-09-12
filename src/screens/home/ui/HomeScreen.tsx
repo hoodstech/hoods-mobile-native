5eff96171a71bc19c1aee3d64dafe18cbcfede49
Roman Bogdanovskiy <romain.bogdanovsky@yandex.ru>
2024-09-12 16:23:08 +0300

    [36949678] feat: Кастомный базовый Input

--- src/screens/home/ui/HomeScreen.tsx
+++ src/screens/home/ui/HomeScreen.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import { KeyboardAvoidingView } from 'react-native'
 
-import { Button, CustomText } from '~/shared/ui'
+import { Button, CustomText, Input } from '~/shared/ui'
 
-export const HomeScreen = () => (
-  <KeyboardAvoidingView behavior="padding">
-    <CustomText variant='h1'>Hoods знает, что вам нужно</CustomText>
-    <Button title="Click me" onPress={() => alert('Button pressed!')} />
-  </KeyboardAvoidingView>
-)
+export const HomeScreen = () => {
+	const [inputValue, setInputValue] = useState('')
+
+	return (
+		<KeyboardAvoidingView behavior="padding">
+			<CustomText variant="h1">Hoods знает, что вам нужно</CustomText>
+			<Button title="Click me" onPress={() => alert('Button pressed!')} />
+			<Input
+				label="test label"
+				value={inputValue}
+				onChangeText={setInputValue}
+				required
+			/>
+		</KeyboardAvoidingView>
+	)
+}
