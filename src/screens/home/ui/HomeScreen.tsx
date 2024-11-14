195362c38cd33cdc41bb442063199cf666bc1e7b
Vladimir Bugaev <robobson@mail.ru>
2024-11-14 20:05:19 +0300

    Выпил кнопки и временное исключение хука

--- src/screens/home/ui/HomeScreen.tsx
+++ src/screens/home/ui/HomeScreen.tsx
@@ -1,14 +1,13 @@
 
 import { KeyboardAvoidingView } from 'react-native'
 
-import { Button, CustomText } from '~/shared/ui'
+import { CustomText } from '~/shared/ui'
 
 export const HomeScreen = () => {
 
 	return (
 		<KeyboardAvoidingView behavior="padding">
 			<CustomText variant="h1">Hoods знает, что вам нужно</CustomText>
-			<Button title="Click me" />
 		</KeyboardAvoidingView>
 	)
 }
